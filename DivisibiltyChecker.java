c9ef590c2fe759212da7aa51ca92d3b0bb8afd71
chinmaisujith <137872974+chinmaisujith@users.noreply.github.com>
2024-04-01 12:08:47 +0530

    Update DivisibiltyChecker.java

--- DivisibiltyChecker.java
+++ DivisibiltyChecker.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class DivisibilityChecker {
+public class DivisibiltyChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
